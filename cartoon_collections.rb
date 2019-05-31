52bd443aa8c4b2a6e067153eb95f52349189590c
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 18:59:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,15 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |list, index|
     puts "#{index + 1}. #{list}"
-
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  veggies.collect do |call|
+    call = call.upper
+    call << ("!")
+  end
 end
 
 def long_planeteer_calls# code an argument here
