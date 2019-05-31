aed6ea1475488ffc1b82946f2b054999da5b1193
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 19:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,10 @@ def long_planeteer_calls(words)# code an argument here
   words.any? { |w| w.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |s|
+    s == cheese_types
+  end
 end
